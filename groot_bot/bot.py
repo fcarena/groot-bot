274808894634a3b1b31b8dad3d2df7438906715d
Anderson Bravalheri <andersonbravalheri@gmail.com>
2017-03-24 20:42:31 -0300

    Finish simple Watson x Telegram integration

--- groot_bot/bot.py
+++ groot_bot/bot.py
@@ -1,4 +1,5 @@
 import yaml
+from functools import wraps
 from datetime import datetime
 from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 from watson_developer_cloud import ConversationV1
@@ -27,34 +28,96 @@ def add_debugger(handler):
     return decorated_handler
 
 
+class TelegramSession(object):
+    def __init__(self, bot, update, chat, user):
+        self.bot = bot
+        self.update = update
+        self.chat, self.user = chat, user
+        self.input_text = update.message.text
+
+    def send(self, messages):
+        print('telegram.send responses:', messages)
+        return [
+            self.bot.send_message(chat_id=self.chat['id'], **message)
+            for message in messages
+        ]
+
+    def send_text(self, messages):
+        if isinstance(messages, str):
+            messages = [messages]
+
+        print('telegram.send_text messages:', messages)
+        return self.send([{'text': text} for text in messages])
+
+
 class ConversationSession(object):
     def __init__(self, user, config):
         keys = getkeys(config['watson'], ['username', 'password', 'version'])
         print('keys:', keys)
         self.conversation = ConversationV1(**keys)
-        print('conversation:', conversation)
-        print('workspaces:', conversation.list_workspaces())
+        print('conversation:', self.conversation)
+        print('workspaces:', self.conversation.list_workspaces())
 
-        self.workspace_id = getpath(config, 'watson.workspace')
+        self.workspace = getpath(config, 'watson.workspace')
         self.context = {}
         self.entities = []
         self.user = user
         self.started_at = datetime.now()
 
-    def x(self):
-        print('inside x')
-
-    def message(self, **kwargs):
+    def send(self, **kwargs):
         print('try conversation', kwargs, self.workspace)
 
         response = self.conversation.message(
-            workspace_id=self.workspace, context=self.context, **kwargs)
+            workspace_id=self.workspace,
+            context=self.context,
+            message_input=kwargs)
         print('Response:', response)
 
-        self.entities += response['entities']
+        self.entities = self.entities + response['entities']
+        print('entities:', self.entities)
         self.context = response['context']
+        print('context:', self.context)
 
         return getpaths(response, ['output.text', 'intents', 'entities'])
+
+    def send_text(self, text):
+        return self.send(text=text)
+
+def restart_session(method):
+    @wraps(method)
+    def _method(self, bot, update):
+        print('restarting session ...')
+        print(type(bot), type(update), getattr(update, 'extract_chat_and_user', 'non-excsite'))
+        _, user = update.extract_chat_and_user()
+
+        self.brain.pop(user['id'], None)
+        print('bot brain removed ...')
+        return method(self, bot, update)
+
+    return _method
+
+def with_session(method):
+    @wraps(method)
+    def _method(self, bot, update):
+        print('with session')
+        chat, user = update.extract_chat_and_user()
+        print('Telegram chat <', chat['id'], '>', 'for user <', user['id'], '>')
+
+        session = self.brain.get(user['id'], None)
+        telegram = TelegramSession(bot, update, chat, user)
+
+        if session is None:
+            conversation = ConversationSession(user, self.config)
+            self.brain[user['id']] = conversation
+        else:
+            conversation = session
+
+        print('Conversation:', conversation)
+        print('Telegram:', telegram)
+
+        return method(self, telegram, conversation)
+
+    return _method
 
 
 class GrootBot(object):
@@ -81,37 +144,23 @@ class GrootBot(object):
         self.updater.start_polling()
         self.updater.idle()
 
-    def recover_session(self, update):
-        user = self.recover_user(update)
-        session = self.brain.get(user['id'], None)
+    @with_session
+    def listen(self, telegram, conversation):
+        responses, intents, entities = conversation.send_text(telegram.input_text)
+        print('response.text:', responses)
+        telegram.send_text(responses)
 
-        if session is None:
-            session = ConversationSession(user, self.config)
-            self.brain[user['id']] = session
+    @restart_session
+    @with_session
+    def start(self, telegram, conversation):
+        responses, intents, entities = conversation.send_text('')
+        print('response.text:', responses)
+        telegram.send_text(responses)
 
-        return session
-
-    def recover_user(self, update):
-        return getpath(update, 'message.from_user')
-
-    def help(self, bot, update):
-        chat, name = getpaths(
-            update, ['message.chat_id', 'message.from_user.first_name'])
-
-        bot.send_message(chat_id=chat, text='Hi {}, I am Groot!'.format(name))
-
-    def listen(self, bot, update):
-        chat, text = getpaths(update, ['message.chat_id', 'message.text'])
-
-        session = self.recover_session(update)
-        print('Session:', session)
-        session.x()
-        session.message(text=text)
-
-        bot.send_message(chat_id=chat, text='Hi {}, I am Groot!'.format(name))
-
-    def start(bot, update):
-        update.message.reply_text("I'm Groot!")
+    @with_session
+    def help(self, telegram, _):
+        telegram.send_text(
+            'Meus criadores não me ensinaram a responder isso...')
 
 def run():
     with open(CONFIG) as handler:
