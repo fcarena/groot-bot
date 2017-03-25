d4b763f1417f127fe914e3a605bbfa279c0142aa
Anderson Bravalheri <andersonbravalheri@gmail.com>
2017-03-25 11:07:09 -0300

    [WIP] Add interface for query action

--- groot_bot/bot.py
+++ groot_bot/bot.py
@@ -1,29 +1,16 @@
 import logging
 import yaml
+
+from functools import wraps
+
+from emoji import emojize
 from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 
 from .getters import getpath
-from .session import restart_session, with_session
+from .conversation import with_conversation
+from .query import QueryService
 
 CONFIG = 'config.yaml'
-
-
-def add_debugger(handler):
-    def _handler(bot, update):
-        logging.debug('<%s> invoked ...\nuser: %s, text: %s',
-                      handler.__name__,
-                      update.message.from_user.first_name,
-                      update.message.text)
-
-        try:
-            response = handler(bot, update)
-        except err:
-            logging.exception(
-                'An error occurred while calling handler: %s', err)
-
-        return response
-
-    return _handler
 
 
 class GrootBot(object):
@@ -50,26 +37,62 @@ class GrootBot(object):
         self.updater.start_polling()
         self.updater.idle()
 
-    @with_session
-    def listen(self, telegram, conversation):
-        responses, intents, entities = conversation.send_text(telegram.input_text)
-        logging.debug('responses: %s', responses)
-        # TODO: lookup considering a context variable 'action'
-        # TODO: call action
-        # TODO: process response
-        telegram.send_text(responses)
+    def create_handler(self, callback, type='message'):
+        @wraps(callback)
+        def _handler(obj, bot, update):
+            chat, user = update.extract_chat_and_user()
+            logging.debug(
+                'callback <%s>, user <%s>, chat <%s>',
+                callback.__name__, user.get('username', user['id']), chat['id']
+            )
+            text = update.extract_message_text()
 
-    @restart_session
-    @with_session
-    def start(self, telegram, conversation):
-        responses, intents, entities = conversation.send_text('')
-        logging.debug('responses: %s', responses)
-        telegram.send_text(responses)
+            return self.proccess_responses(bot, chat, callback(obj, user, text))
 
-    @with_session
-    def help(self, telegram, _):
-        telegram.send_text(
-            'Meus criadores não me ensinaram a responder isso...')
+        return _handler
+
+    def process_responses(self, bot, chat, responses):
+        if isinstance(response, dict):
+            responses = [responses]
+
+        logging.debug('%d responses', len(responses))
+        return [self.process_response(response) for response in responses]
+
+    def process_response(self, bot, chat, response):
+        if isinstance(response, str):
+            response = dict(text=text, parse_mode='HTML')
+
+        text = response.get('text', None)
+        if text is not None:
+            response['text'] = emojize(text, use_aliases=True)
+
+        logging.debug('response: %s', response)
+        return bot.send_message(chat_id=chat['id'], **response)
+
+    @with_conversation
+    def answer(self, user, text, conversation):
+        responses = conversation.send(text=text, context=dict(user=user))
+        logging.debug('responses: %s', responses)
+
+        text = getpath(responses, 'output.text')
+        if text is None:
+            raise ValueError('Conversation response should not be empty')
+
+        action = responses['context'].get('action', None)
+        if action == 'query':
+            query = QueryService(self.config)
+            text = query(text)
+
+        return text
+
+    def start(self, user, _):
+        logging.info('Restarting bot brain ...')
+        self.brain[user['id']] = {}
+
+        return self.listen(user, '')
+
+    def help(self, user, _):
+        return dict(text='Meus criadores não me ensinaram a responder isso...')
 
 def run():
     logging.basicConfig(
---
+++ groot_bot/conversation.py
@@ -0,0 +1,57 @@
+import logging
+
+from datetime import datetime
+from functools import wraps
+
+from watson_developer_cloud import ConversationV1
+
+from .getters import getpath, getpaths, getkeys
+
+
+def with_conversation(method):
+    @wraps(method)
+    def _method(groot, user, text):
+        logging.debug('Retrieving Watson conversation interface...')
+
+        session = groot.brain.get(user['id'], {})
+        conversation = (
+            session.get('conversation', None) or
+            ConversationSession(user, self.config)
+        )
+
+        logging.debug('conversation: %s', conversation)
+
+        return method(groot, user, text, conversation)
+
+    return _method
+
+
+class ConversationSession(object):
+    def __init__(self, user, config):
+        keys = getkeys(config['watson'], ['username', 'password', 'version'])
+        logging.debug('keys:', keys)
+        self.conversation = ConversationV1(**keys)
+        logging.debug('conversation: %s', self.conversation)
+        logging.debug('workspaces: %s', self.conversation.list_workspaces())
+
+        self.workspace = getpath(config, 'watson.workspace')
+        self.context = {}
+        self.entities = []
+        self.user = user
+        self.started_at = datetime.now()
+
+    def send(self, **kwargs):
+        logging.debug('kwargs: %s, workspace: %s', kwargs, self.workspace)
+
+        response = self.conversation.message(
+            workspace_id=self.workspace,
+            context=self.context,
+            message_input=kwargs)
+        logging.debug('response: %s', response)
+
+        self.entities = self.entities + response['entities']
+        logging.debug('entities: %s', self.entities)
+        self.context = response['context']
+        logging.debug('context: %s', self.context)
+
+        return getpaths(response, ['output.text', 'intents', 'entities'])
